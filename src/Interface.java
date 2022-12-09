1ae18cf99ac182d10fddd102dc838f7005c614aa
Joris <joriscourteaux@gmail.com>
2022-12-09 09:10:31 +0100

    Zoom v1

--- src/Interface.java
+++ src/Interface.java
@@ -2,7 +2,9 @@ import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 public class Interface extends Application {
     @Override
@@ -21,6 +23,22 @@ public class Interface extends Application {
         camera.setFarClip(2000.0);
         camera.setFieldOfView(35);
         ihm.setCamera(camera);
+        ihm.addEventHandler(MouseEvent.ANY, event -> {
+            if (event.getEventType() == MouseEvent.MOUSE_PRESSED) {
+                System.out.println("Clicked on : (" + event.getSceneX()+ ", "+ event.getSceneY()+ ")");
+                double base = event.getSceneY();
+            }
+            if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
+                double diff = (event.getSceneY() - 200)/10;
+                camera.getTransforms().add(new Translate(0,0,diff));
+                System.out.println(diff);// A vous de compl´eter
+                //System.out.println("dragged on : (" + event.getSceneX()+ ", "+ event.getSceneY()+")");
+            }
+
+
+
+        });
+
         primaryStage.setScene(ihm);
         primaryStage.show();
     }
