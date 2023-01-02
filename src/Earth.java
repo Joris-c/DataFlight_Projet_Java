4d67282fcd7b50e01cf26240059f585243dda9f0
Joris <joriscourteaux@gmail.com>
2023-01-02 13:54:24 +0100

    update tp

--- src/Earth.java
+++ src/Earth.java
@@ -1,25 +1,31 @@
 import javafx.animation.AnimationTimer;
+import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Box;
+import javafx.scene.shape.CullFace;
 import javafx.scene.shape.Sphere;
 import javafx.scene.transform.Rotate;
 
 import java.io.File;
 import java.net.MalformedURLException;
+import java.util.List;
 
 public class Earth extends Group {
-
     private Sphere sph = new Sphere(300);
+    private Sphere s = new Sphere(2);
+    boolean behindEarth = false;
+
     private Rotate ry = new Rotate();
+    private Rotate rx = new Rotate();
     private PhongMaterial map = new PhongMaterial();
 
     private  final double R = 300;
 
-
-
+    private int var = 0;
 
     public Earth() {
         try {
@@ -27,38 +33,77 @@ public class Earth extends Group {
         } catch (MalformedURLException e) {
             throw new RuntimeException(e);
         }
+
         ry.setAxis(Rotate.Y_AXIS);
+        rx.setAxis(Rotate.X_AXIS);
+        this.sph.toFront();
         this.sph.setMaterial(map);
         this.getChildren().add(sph);
         this.getTransforms().add(ry);
+        this.getTransforms().add(rx);
         //sph.getTransforms().add(ry);
-
         AnimationTimer animationTimer = new AnimationTimer() {
             @Override
             public void handle(long time) {
                 ry.setAngle(360*(time*1e-9)/15);
+                double z_airport = s.localToScene(s.getTranslateX(), s.getTranslateY(), s.getTranslateZ()).getZ();
+                double z_earth = localToScene(sph.getTranslateX(), sph.getTranslateY(), sph.getTranslateZ()).getZ();
+                if(z_airport>(z_earth-110)){
+                    behindEarth = false;
+                }else {
+                    behindEarth = true;
+                }
+                s.setVisible(behindEarth);
+
+
+
             }
         };
         animationTimer.start();
     }
 
     public Sphere createSphere(Aeroport a, Color color){
-        Sphere s = new Sphere(10);
-        s.setTranslateX(150 * Math.cos(Math.toRadians(a.getLatitude()-13))*Math.sin(Math.toRadians(a.getLongitude())));
-        s.setTranslateY(-150 * Math.sin(Math.toRadians(a.getLatitude()-13)));
-        s.setTranslateZ(-150 * Math.cos(Math.toRadians(a.getLatitude()-13))*Math.sin(Math.toRadians(a.getLongitude())));
+
+        this.s.setTranslateX(300 * Math.cos(Math.toRadians(a.getLatitude() * 0.65)) * Math.sin(Math.toRadians(a.getLongitude())));
+        this.s.setTranslateY(-300 * Math.sin(Math.toRadians(a.getLatitude() * 0.65)));
+        this.s.setTranslateZ(-300 * Math.cos(Math.toRadians(a.getLatitude() * 0.65)) * Math.cos(Math.toRadians(a.getLongitude())));
         /*s.setTranslateX(150 * Math.cos(Math.toRadians(a.getLatitude()-13))*Math.sin(Math.toRadians(a.getLongitude()-13)));
         s.setTranslateY(-150 * Math.sin(Math.toRadians(a.getLatitude()-13)));
         s.setTranslateZ(-150 * Math.cos(Math.toRadians(a.getLatitude()-13))*Math.sin(Math.toRadians(a.getLongitude()-13)));*/
-        s.setMaterial(new PhongMaterial(color));
-        return s;
+        this.s.setMaterial(new PhongMaterial(color));
+        return this.s;
     }
 
     public void displayRedSphere(Aeroport a){
-        this.getChildren().add(createSphere(a,Color.RED));
+        if(a != null) {
+            if(this.getManagedChildren().contains(this.s)){
+                createSphere(a, Color.RED);
+            }
+            else {
+                this.getChildren().add(createSphere(a, Color.RED));
+            }
+
+        }
+        else {
+            throw new IllegalArgumentException("Aeroport null");
+        }
     }
 
 
+    public void Earth_rotate_X(boolean x){
+        int i = 0;
+        while (i<20){
+            if(x){
+                rx.setAngle(rx.getAngle()+(1));
+                i++;
+            }
+            else {
+                rx.setAngle(rx.getAngle()-(1));
+                i++;
+            }
 
+        }
+
+    }
 
 }
--- src/Interface.java
+++ src/Interface.java
@@ -1,36 +1,55 @@
 import javafx.application.Application;
 import javafx.geometry.Point2D;
 import javafx.geometry.Point3D;
+import javafx.geometry.Side;
 import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
-import javafx.scene.input.MouseButton;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.input.PickResult;
-import javafx.scene.input.ScrollEvent;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.*;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.Paint;
 import javafx.scene.transform.Translate;
 import javafx.stage.Stage;
 
 import java.util.concurrent.atomic.AtomicReference;
 
+//API KEY : 367de72a690d5732c70ac8f4dfeb6737
 public class Interface extends Application {
 
     World w = new World("src/ressource/airport-codes_no_comma.csv");
-
+    double z_max = -500;
+    double z_min = -1500;
+    double current_z = -1000;
     @Override
     public void start(Stage primaryStage) throws Exception{
         AtomicReference<Double> base = new AtomicReference<>((double) 0);
         primaryStage.setTitle("Hello world");
         Earth earth = new Earth();
-        Pane pane = new Pane(earth);
+        BackgroundImage bg = new BackgroundImage(new Image("/ressource/bg_voie_lactee.jpg")
+                , BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+                BackgroundSize.DEFAULT);
+
+        Pane pane = new Pane();
+        pane.getChildren().add(prepareBG());
+        pane.getChildren().add(earth);
+        //pane.setBackground(new Background(bg));
+
+        ImageView bg1 = new ImageView();
+        bg1.setImage(new Image("https://www.science-et-vie.com/wp-content/uploads/scienceetvie/2020/05/d-voie-lactee-est-elle-visible.jpg"));
         Scene ihm = new Scene(pane, 600, 400,false);
 
+        //ihm.setFill(Color.GRAY);
+
+
+        //ihm.setFill(Color.GREEN);
 
         PerspectiveCamera camera = new PerspectiveCamera(true);
         camera.setTranslateZ(-1000);
         camera.setNearClip(0.1);
-        camera.setFarClip(2000.0);
+        camera.setFarClip(3000.0);
         camera.setFieldOfView(35);
         ihm.setCamera(camera);
         ihm.addEventHandler(MouseEvent.ANY, event -> {
@@ -40,8 +59,12 @@ public class Interface extends Application {
             }
             if (event.getEventType() == MouseEvent.MOUSE_DRAGGED) {
                 double diff = (event.getSceneY() - base.get());
-                camera.getTransforms().add(new Translate(0,0,diff));
-                base.set(event.getSceneY());
+                if(current_z + diff < z_max && current_z + diff > z_min){
+                    camera.getTransforms().add(new Translate(0,0,diff));
+                    base.set(event.getSceneY());
+                    current_z += diff;
+                }
+
                 //System.out.println("dragged on : (" + event.getSceneX()+ ", "+ event.getSceneY()+")");
             }
             if (event.getButton()== MouseButton.SECONDARY && event.getEventType()==MouseEvent.MOUSE_CLICKED) {
@@ -65,11 +88,35 @@ public class Interface extends Application {
             }
 
         });
+        ihm.addEventHandler(KeyEvent.KEY_PRESSED, keyEvent -> {
+            if(keyEvent.getText().equals("+")){
+                earth.Earth_rotate_X(true);
+                System.out.println("up");
+            }
+            if(keyEvent.getText().equals("-")){
+                earth.Earth_rotate_X(false);
+                System.out.println("down");
+            }
+        });
 
         primaryStage.setScene(ihm);
         primaryStage.show();
+
+
     }
     public static void main(String[] args) {
         launch(args);
     }
+
+    private ImageView prepareBG(){
+        Image image = new Image("/ressource/bg_voie_lactee.jpg",2304,1680,false,false);
+        System.out.println("image.getWidth() = " + image.getWidth());
+        System.out.println("image.getHeight() = " + image.getHeight());
+        ImageView imageView = new ImageView(image);
+        imageView.setPreserveRatio(true);
+        imageView.getTransforms().add(new Translate(-image.getWidth()/2,-image.getHeight()/2,800));
+        return imageView;
+    }
 }
+
+
